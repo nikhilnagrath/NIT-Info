4b111419d57f2c3d827389877607b4bc98d876e9
Nikhil Nagrath <37298937+nikhilnagrath@users.noreply.github.com>
2018-04-19 13:34:10 +0530

    Add files via upload

--- main.c
+++ main.c
@@ -1,144 +1,53 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-struct NITIndia{
-        int area;
-        char state[20];
-        int year;
-        char director_name[30];
-        int ranking;
-    }NITT,NITR,NITSurat,NITW,NITK,MNNIT,NITC,NITJ,NITS,NITH;
-
 int main()
 {
-    int rank,repeat=1;
+    int sem,rolls1,rolls2,rolls3,repeat=1;
     while(repeat==1){
-        printf("Write the rank of NIT from 1 to 10 to get its details\n");
-        scanf("%d",&rank);
-        switch(rank){
-        case 1: printf("NIT Tiruchirappalli\n");
-                NITT.area=800;
-                printf("Size: %d acres\n",NITT.area);
-                strcpy(NITT.state,"Tamil Nadu");
-                printf("State: %s\n",NITT.state);
-                strcpy(NITT.director_name,"Mini Shaji Thomas");
-                printf("Director Name: %s\n",NITT.director_name);
-                NITT.year=2003;
-                printf("Converted into NIT on: %d\n",NITT.year);
-                NITT.ranking=1;
-                printf("Rank: %d\n",NITT.ranking);
-        break;
-        case 2: printf("NIT Rourkela\n");
-                NITR.area=648;
-                printf("Size: %d acres\n",NITR.area);
-                strcpy(NITR.state,"Odisha");
-                printf("State: %s\n",NITR.state);
-                strcpy(NITR.director_name,"Animesh Biswas");
-                printf("Director Name: %s\n",NITR.director_name);
-                NITR.year=2002;
-                printf("Converted into NIT on: %d\n",NITR.year);
-                NITR.ranking=2;
-                printf("Rank: %d\n",NITR.ranking);
-        break;
-        case 3: printf("NIT Surathkal\n");
-                NITSurat.area=295;
-                printf("Size: %d acres\n",NITSurat.area);
-                strcpy(NITSurat.state,"Karnataka");
-                printf("State: %s\n",NITSurat.state);
-                strcpy(NITSurat.director_name,"Karanam Uma Maheshwar Rao");
-                printf("Director Name: %s\n",NITSurat.director_name);
-                NITSurat.year=2002;
-                printf("Converted into NIT on: %d\n",NITSurat.year);
-                NITSurat.ranking=3;
-                printf("Rank: %d\n",NITSurat.ranking);
-        break;
-        case 4: printf("NIT Warangal\n");
-                NITW.area=256;
-                printf("Size: %d acres\n",NITW.area);
-                strcpy(NITW.state,"Telangana");
-                printf("State: %s\n",NITW.state);
-                strcpy(NITW.director_name,"N. V. Ramana Rao[");
-                printf("Director Name: %s\n",NITW.director_name);
-                NITW.year=2002;
-                printf("Converted into NIT on: %d\n",NITW.year);
-                NITW.ranking=4;
-                printf("Rank: %d\n",NITW.ranking);
-        break;
-        case 5: printf("NIT Kurukshetra\n");
-                NITK.area=300;
-                printf("Size: %d acres\n",NITK.area);
-                strcpy(NITK.state,"Haryana");
-                printf("State: %s\n",NITK.state);
-                strcpy(NITK.director_name,"Satish Kumar");
-                printf("Director Name: %s\n",NITK.director_name);
-                NITK.year=2002;
-                printf("Converted into NIT on: %d\n",NITK.year);
-                NITK.ranking=5;
-                printf("Rank: %d\n",NITK.ranking);
-        break;
-        case 6: printf("MNNIT Allahabad\n");
-                MNNIT.area=222;
-                printf("Size: %d acres\n",MNNIT.area);
-                strcpy(MNNIT.state,"Allahabad");
-                printf("State: %s\n",MNNIT.state);
-                strcpy(MNNIT.director_name,"	Dr. Rajeev Tripathi");
-                printf("Director Name: %s\n",MNNIT.director_name);
-                MNNIT.year=2007;
-                printf("Converted into NIT on: %d\n",MNNIT.year);
-                MNNIT.ranking=6;
-                printf("Rank: %d\n",MNNIT.ranking);
-        break;
-        case 7: printf("NIT Calicut\n");
-                NITC.area=300;
-                printf("Size: %d acres\n",NITC.area);
-                strcpy(NITC.state,"Kerala");
-                printf("State: %s\n",NITC.state);
-                strcpy(NITC.director_name,"	Dr. Sivaji Chakravorti");
-                printf("Director Name: %s\n",NITC.director_name);
-                NITC.year=2002;
-                printf("Converted into NIT on: %d\n",NITC.year);
-                NITC.ranking=7;
-                printf("Rank: %d\n",NITC.ranking);
-        break;
-        case 8: printf("NIT Jaipur\n");
-                NITJ.area=317;
-                printf("Size: %d acres\n",NITJ.area);
-                strcpy(NITJ.state,"Rajasthan");
-                printf("State: %s\n",NITJ.state);
-                strcpy(NITJ.director_name,"	Professor Udaykumar R Yaragatti");
-                printf("Director Name: %s\n",NITJ.director_name);
-                NITJ.year=2002;
-                printf("Converted into NIT on: %d\n",NITJ.year);
-                NITJ.ranking=8;
-                printf("Rank: %d\n",NITJ.ranking);
-        break;
-        case 9: printf("NIT Silchar\n");
-                NITS.area=625;
-                printf("Size: %d acres\n",NITS.area);
-                strcpy(NITS.state,"Assam");
-                printf("State: %s\n",NITS.state);
-                strcpy(NITS.director_name,"Sivaji Bandyopadhyay");
-                printf("Director Name: %s\n",NITS.director_name);
-                NITS.year=2002;
-                printf("Converted into NIT on: %d\n",NITS.year);
-                NITS.ranking=9;
-                printf("Rank: %d\n",NITS.ranking);
-        break;
-        case 10: printf("NIT Hamirpur\n");
-                NITH.area=320;
-                printf("Size: %d acres\n",NITH.area);
-                strcpy(NITH.state,"Himachal Pradesh");
-                printf("State: %s\n",NITH.state);
-                strcpy(NITH.director_name,"Prof. (Dr.) Vinod Yadava");
-                printf("Director Name: %s\n",NITH.director_name);
-                NITH.year=2002;
-                printf("Converted into NIT on: %d\n",NITH.year);
-                NITH.ranking=10;
-                printf("Rank: %d\n",NITH.ranking);
-        break;
-        }
-        printf("Choose an option whether you want see info of any other NIT or end the program\n1:continue\n2:end\n");
-        scanf("%d",&repeat);
+    printf("Welcome to CGPA Displayer\n");
+    printf("Write the Semester of which you want to see CGPA\n1:1st Semester\n2:2nd Semester\n3:3rd Semester\n");
+    scanf("%d",&sem);
+    switch(sem){
+    case 1: printf("You selected 1st Semester\n");
+            printf("Write the Roll No. of the Student to see his/her CGPA\n1:Nikhil\n2:Yash\n3:Rashmi\n");
+            scanf("%d",&rolls1);
+            switch(rolls1){
+            case 1: printf("Nikhil\nCGPA:8.3\n");
+            break;
+            case 2: printf("Yash\nCGPA:8.5\n");
+            break;
+            case 3: printf("Rashmi\nCGPA:9.0\n");
+            break;
+            }
+    break;
+    case 2: printf("You selected 2nd Semester\n");
+            printf("Write the Roll No. of the Student to see his/her CGPA\n1:Vaibhav\n2:Gaurav\n3:Prabhnoor\n");
+            scanf("%d",&rolls2);
+            switch(rolls2){
+            case 1: printf("Vaibhav\nCGPA:8.7\n");
+            break;
+            case 2: printf("Gaurav\nCGPA:8.1\n");
+            break;
+            case 3: printf("Prabhnoor\nCGPA:9.3\n");
+            break;
+            }
+    break;
+    case 3: printf("You selected 3rd Semester\n");
+            printf("Write the Roll No. of the Student to see his/her CGPA\n1:Shivam\n2:Sahil\n3:Nupur\n");
+            scanf("%d",&rolls3);
+            switch(rolls3){
+            case 1: printf("Shivam\nCGPA:8.8\n");
+            break;
+            case 2: printf("Sahil\nCGPA:8.0\n");
+            break;
+            case 3: printf("Nupur\nCGPA:8.6\n");
+            break;
+            }
+    break;
+    }
+    printf("Choose an option whether you want to end or see another Student's CGPA\n1:Continue\n2:End\n");
+    scanf("%d",&repeat);
     }
 return 0;
 }
